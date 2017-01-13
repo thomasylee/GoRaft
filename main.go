f612925c0f32f0ca3e44395fcac10bffbd1ba59b
Thomas Lee <tomyl@protonmail.com>
2017-01-12 23:27:57 -0800

    Rename heartbeat_timeout to election_timeout and add jitter

--- main.go
+++ main.go
@@ -3,12 +3,24 @@ package main
 import (
 	"io/ioutil"
 	"log"
+	"math/rand"
 	"time"
 
 	"github.com/go-yaml/yaml"
 
 	"github.com/thomasylee/GoRaft/api"
 )
+
+/**
+ * Returns a timeout value between average - jitter and average + jitter.
+ */
+func calc_timeout(average int, jitter int) int {
+	if jitter == 0 {
+		return average
+	}
+
+	return average - jitter + rand.Intn(2 * jitter)
+}
 
 /**
  * The main() method is the initial method that gets run, so it will start
@@ -29,12 +41,16 @@ func main() {
 	timeoutChannel := make(chan bool)
 	go api.RunServer(timeoutChannel, config["api_port"].(int))
 
-	timeout := time.Duration(config["heartbeat_timeout"].(int))
+	// Randomize the election timeout to minimize the risk of two nodes
+	// initiating an election at the same time.
+	election_timeout := config["election_timeout"].(int)
+	election_timeout_jitter := config["election_timeout_jitter"].(int)
 	for {
+		timeout := calc_timeout(election_timeout, election_timeout_jitter)
 		select {
 		case <-timeoutChannel:
 			// Do nothing since we didn't time out.
-		case <-time.After(timeout * time.Millisecond):
+		case <-time.After(time.Duration(timeout) * time.Millisecond):
 			// TODO: Start leader election process.
 		}
 	}
