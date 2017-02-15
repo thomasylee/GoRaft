cf7a9e6686d622689fff0c3c6fa8bd3a4b151d2a
Thomas Lee <tomyl@protonmail.com>
2017-02-14 22:39:02 -0800

    Fix index error in NodeState test

--- state/node_state_test.go
+++ state/node_state_test.go
@@ -33,9 +33,46 @@ func Test_SetLogEntry_WithValidNodeAndParams_SetsEntryInMemAndStateMachine(t *te
 	for _, test := range tests {
 		node.SetLogEntry(test.index, test.entry)
 
-		// Log entry indices start at 1, but the entryInMem slice indices start at 0,
-		// so we must add 1 to make the indices align.
-		entryInMem := node.Log(test.index + 1)
+		entryInMem := node.Log(test.index)
+		if entryInMem != test.entry {
+			t.Error("Log entry in memory doesn't match:", test.index, entryInMem)
+		}
+
+		jsonInSM, err := node.NodeStateMachine.Get(strconv.Itoa(int(test.index)))
+		if err != nil {
+			t.Errorf("Error processing entry %d: %s", test.index, err.Error())
+		} else if jsonInSM != test.jsonRep {
+			t.Error("Log entry JSON in state machine doesn't match:", test.index, jsonInSM)
+		}
+	}
+}
+
+func Test_SetLogEntry_WithExistingIndex_SetsEntryInMemAndStateMachine(t *testing.T) {
+	global.SetUpLogger()
+	global.SetLogLevel("critical")
+
+	node := createNodeState()
+	node.log = &[]LogEntry{}
+
+	var tests = []struct {
+		index uint32
+		entry LogEntry
+		jsonRep string
+	}{
+		{1, LogEntry{"a", "A", 0}, "{\"Key\":\"a\",\"Value\":\"A\",\"Term\":0}"},
+		{2, LogEntry{"b", "B", 0}, "{\"Key\":\"b\",\"Value\":\"B\",\"Term\":0}"},
+		{3, LogEntry{"c", "C", 1}, "{\"Key\":\"c\",\"Value\":\"C\",\"Term\":1}"},
+	}
+
+	for _, test := range tests {
+		node.SetLogEntry(test.index, test.entry)
+	}
+
+	tests[1].jsonRep = "{\"Key\":\"abc\",\"Value\":\"ABC\",\"Term\":1}"
+	node.SetLogEntry(2, LogEntry{"abc", "ABC", 1})
+
+	for _, test := range tests {
+		entryInMem := node.Log(test.index)
 		if entryInMem != test.entry {
 			t.Error("Log entry in memory doesn't match:", test.index, entryInMem)
 		}
