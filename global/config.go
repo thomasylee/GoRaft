b1f504ac3417a1ca6decf9f1bb6ac011fa6f5fa8
Thomas Lee <tomyl@protonmail.com>
2017-02-17 01:11:24 -0800

    Add leader_heartbeat_period to config

--- global/config.go
+++ global/config.go
@@ -16,29 +16,14 @@ type ConfigMap struct {
 	LogLevel string `yaml:"log_level"`
 	ElectionTimeout uint32 `yaml:"election_timeout"`
 	ElectionTimeoutJitter uint32 `yaml:"election_timeout_jitter"`
+	LeaderHeartbeatPeriod uint32 `yaml:"leader_heartbeat_period"`
 	NodeId string `yaml:"node_id"`
 	Nodes map[string]NodeHost `yaml:"node_hosts"`
 }
 
 var Config ConfigMap
 
-/**
- * Load the config map from config.yaml.
- */
-/*
-func LoadConfig() map[interface{}]interface{} {
-	configData, err := ioutil.ReadFile("config.yaml")
-	if err != nil {
-		Log.Panic(err)
-	}
-	configYaml := string(configData)
-
-	Config = make(map[interface{}]interface{})
-	yaml.Unmarshal([]byte(configYaml), &Config)
-	return Config
-}
-*/
-
+// Load the config map from config.yaml.
 func LoadConfig() ConfigMap {
 	configData, err := ioutil.ReadFile("config.yaml")
 	if err != nil {
